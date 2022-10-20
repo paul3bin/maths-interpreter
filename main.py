eafb4ccaf28a377954e89e48b5e165501fbdb3c3
Ebin Paul <ebinpaul@outlook.com>
2022-10-20 13:43:21 +0100

    added displaying list of token functionality

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Authors: Max James, Christopher Gavey, Ebin Paul, Aswin, Soniya
+Program: Mathematical visualisation software interpreter
+Version: 1.01
+Version Date: 19/10/2022
+"""
+from core.lexer.lexicalAnalyzer import Lexer
+
+while True:
+    input_string = input("Enter expression >> ")
+    lexer = Lexer(input_string)
+
+    tokens = lexer.get_tokens()
+
+    print(tokens)
