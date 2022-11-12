17e5664bf6a8b5321513257b85e44914ce5afc60
Ebin Paul <ebinpaul@outlook.com>
2022-11-12 13:26:34 +0000

    refactored code for accessing main

--- core/interpreter.py
+++ core/interpreter.py
@@ -1,4 +1,6 @@
 from core.parser.nodes import Token, TokenType
+from .lexer.lexicalAnalyzer import Lexer
+from .parser.syntaxAnalyzer import Parser
 
 
 class Interpreter:
@@ -7,3 +9,20 @@ class Interpreter:
 
     def execute(self):
         return self.__root_node.get_node_value()
+
+
+def main(input_string: str):
+    lexer = Lexer(input_string)
+
+    tokens = lexer.get_tokens()
+    # result = lexer.get_tokens()
+
+    parser = Parser(tokens)
+
+    root_node = parser.parse()
+
+    interpreter = Interpreter(root_node)
+
+    result = interpreter.execute()
+
+    return result
--- main.py
+++ main.py
@@ -4,26 +4,7 @@ Program: Mathematical visualisation software interpreter
 Version: 1.1
 Version Date: 08/11/2022
 """
-from core.interpreter import Interpreter
-from core.lexer.lexicalAnalyzer import Lexer
-from core.parser.syntaxAnalyzer import Parser
-
-
-def main(input_string: str):
-    lexer = Lexer(input_string)
-
-    tokens = lexer.get_tokens()
-
-    parser = Parser(tokens)
-
-    root_node = parser.parse()
-
-    interpreter = Interpreter(root_node)
-
-    result = interpreter.execute()
-
-    return result
-
+from core.interpreter import main
 
 if __name__ == "__main__":
     while True:
