ae6889a427a2c083c7ab353572251d2f344015b1
Ebin Paul <ebinpaul@outlook.com>
2022-11-22 11:03:36 +0000

    sorted imports and changed interpreter object creation to passing input string to main function

--- core/functions/zero_crossing.py
+++ core/functions/zero_crossing.py
@@ -1,20 +1,17 @@
-# from ..interpreter import Interpreter
-# from core.interpreter import Interpreter
-# from .. import Interpreter
-from interpreter import *
+from ..interpreter import main
 
 
 def derivative(expression: str, xval: str):
-    d1 = Interpreter(f"d1=x+{0.000001}").execute()
-    d2 = Interpreter(f"d2=x-{0.000001}").execute()
+    d1 = main(f"d1=x+{0.000001}")
+    d2 = main(f"d2=x-{0.000001}")
 
     newExpression1 = expression.replace("x", "d1")
-    expr1 = Interpreter(newExpression1)
-    dans1 = expr1.execute()
+    expr1 = main(newExpression1)
+    dans1 = expr1
 
     newExpression2 = expression.replace("x", "d2")
-    expr2 = Interpreter(newExpression2)
-    dans2 = expr2.execute()
+    expr2 = main(newExpression2)
+    dans2 = expr2
 
     dans3 = (dans1 - dans2) / 2 * 0.000001
     return dans3
@@ -22,40 +19,35 @@ def derivative(expression: str, xval: str):
 
 def zeroCrossing(expression: str, x_value: str):
     # Assign the value of x in the interpreter
-    val = Interpreter(f"x={x_value}")
-    xval = val.execute()
+    val = main(f"x={x_value}")
+    xval = val
     # Pass the expression
-    expr = Interpreter(expression)
-    ans = expr.execute()
+    expr = main(expression)
+    ans = expr
 
     for _ in range(100):
         ans1 = abs(ans)
         # Assign the value of v1
-        val1 = Interpreter(f"v1={ans1}").execute()
+        val1 = main(f"v1={ans1}")
         # compare the value through interpreter
-        if Interpreter("v1<0.00001").execute():
+        if main("v1<0.00001"):
             break
 
         # call the derivative function
         d_value = derivative(expression, xval)
         ans2 = abs(d_value)
         # Assign the value of v2
-        val2 = Interpreter(f"v2={ans2}").execute()
-        # compare the value through interpreter
-        if Interpreter("v2<0.00001").execute():
+        val2 = main(f"v2={ans2}")
+        # compare the value through main
+        if main("v2<0.00001"):
             break
 
         # update the value of xval and x (x of interpreter)***********
-        ans3 = Interpreter(f"x=x-{ans/d_value}")
-        xval = ans3.execute()
+        ans3 = main(f"x=x-{ans}/{d_value}")
+        xval = ans3
 
-        expr = Interpreter(expression)
-        ans = expr.execute()
+        expr = main(expression)
+        ans = expr
 
-    ans4 = Interpreter(f"x={xval}").execute()
+    ans4 = main(f"x={xval}")
     return ans4
-
-
-if __name__ == "__main__":
-    final1 = zeroCrossing("x^2-25", "8")
-    print("\n Zero Crossing at x = {}".format(final1))
--- main.py
+++ main.py
@@ -4,12 +4,15 @@ Program: Mathematical visualisation software interpreter
 Version: 2
 Version Date: 08/11/2022
 """
+from core.functions.zero_crossing import zeroCrossing
 from core.interpreter import main
 
 if __name__ == "__main__":
-    while True:
-        input_string = input("Enter expression >> ")
-        if input_string:
-            print(main(input_string))
+    # while True:
+    #     input_string = input("Enter expression >> ")
+    #     if input_string:
+    #         print(main(input_string))
 
-        continue
+    #     continue
+    final1 = zeroCrossing("x^2-25", "8")
+    print(f"\n Zero Crossing at x = {final1}")
